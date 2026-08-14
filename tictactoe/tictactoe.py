c1b00638aa8c6990406a2615deffd9f4c43f1973
rafaelmnf <rafaelnfilho@gmail.com>
2026-08-13 21:49:46 -0300

    AI for tic tac toe

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -73,12 +73,13 @@ def result(board, action):
     """
 
     # If action is not a valid action for the board, raise an error. Se a ação for diferente de EMPTY, ela já está preenchida
-    if board[action[0]][action[1]] != EMPTY:
-        raise ValueError("Invalid action: position already taken")
+    if action not in actions(board):
+        raise ValueError("Ação inválida para este tabuleiro!")
      
     # Copia o tabuleiro para um novo totalmente individual, ou seja, com endereços novos na memória para poder testar a situação sem mudar o original
     boardWithAction = copy.deepcopy(board)
     boardWithAction[action[0]][action[1]] = player(board)
+
     return boardWithAction
 
 
@@ -88,25 +89,155 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+
+    # Linhas e Colunas
+    playerXdiagonalL = 0
+    playerOdiagonalL = 0
+    playerXdiagonalR = 0
+    playerOdiagonalR = 0
+    k = 2
+
+    for i in range(3):
+        playerXline = 0
+        playerOline = 0
+        playerXcol = 0
+        playerOcol = 0
+
+        for j in range(3):
+            if board[i][j] == X: # Verifica cada linha de X
+                playerXline += 1
+            if board[i][j] == O: # Verifica cada linha de O
+                playerOline += 1            
+            if board[j][i] == X: # Verifica cada coluna de X
+                playerXcol += 1
+            if board[j][i] == O: # Verifica cada coluna de O
+                playerOcol += 1
+
+        # Verifica vencedor linha e coluna
+        if  playerXline == 3 or playerXcol == 3: 
+            return X
+        if  playerOline == 3 or playerOcol == 3:
+            return O
+
+        # Verifica Diagonais 
+        if board[i][i] == X: 
+            playerXdiagonalL += 1
+        if board[i][i] == O: 
+            playerOdiagonalL += 1
+        if board[i][k] == X:
+            playerXdiagonalR += 1
+        if board[i][k] == O:
+            playerOdiagonalR += 1
+        k -= 1
+
+    if playerXdiagonalL == 3 or playerXdiagonalR == 3:
+        return X
+
+    if playerOdiagonalL == 3 or playerOdiagonalR == 3:
+        return O
+
+    # Se ninguém ganhou
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    # Se houver vencedor
+    if winner(board) in (X, O): 
+        return True
+    # Se ainda existe pelo menos uma casa EMPTY, o jogo NÃO acabou
+    for row in board:
+        if EMPTY in row:
+            return False
+
+    # Se não há vencedor nem casas vazias, é empate (jogo acabou)
+    return True
+     
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+
+    if winner(board) == X:
+        return 1
+    if winner(board) == O:
+        return -1
+    return 0
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+
+    # Para o jogador X, sendo este +1 como vitória
+    if player(board) == X:
+        bestValue = -math.inf
+        bestAction = None
+        
+        for action in actions(board): # Para cada ação possível de se fazer na sua vez, devolve aquela em que vc ganha / se sai melhor 
+            move_val = minValue(result(board, action))
+
+            if move_val > bestValue:
+                bestValue = move_val
+                bestAction = action
+
+            if bestValue == 1:
+                return bestAction
+        return bestAction
+
+    else: # Para o jogador O, sendo este -1 como vitória
+        bestValue = math.inf
+        bestAction = None
+        
+        for action in actions(board): # Para cada ação possível de se fazer na sua vez, devolve aquela em que vc ganha / se sai melhor 
+            move_val = maxValue(result(board, action))
+
+            if move_val < bestValue:
+                bestValue = move_val
+                bestAction = action
+
+            if bestValue == -1:
+                return bestAction
+            
+        return bestAction
+
+def minValue(board):
+    """
+    Retorna o menor valor possível dentre os maiores para cada jogada
+    """
+    if terminal(board):
+        return utility(board) # Aqui encerra a recursao de chamadas após verificar todas as possíveis jogadas
+
+    value = math.inf
+
+    for action in actions(board): # Para cada possível jogada
+        value = min(value, maxValue(result(board, action))) # compara os valores para poder retornar o menor deles
+        if value == -1: # Otimiza a função
+            return value
+
+    return value
+
+
+
+def maxValue(board):
+    """
+    Retorna o maior valor possível dentre os menores para cada jogada
+    """
+    if terminal(board):
+        return utility(board)
+
+    value = -math.inf
+    
+    for action in actions(board): 
+        value = max(value, minValue(result(board, action)))
+        if value == 1:
+            return value
+
+    return value
